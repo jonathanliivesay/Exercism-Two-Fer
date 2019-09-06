edc92418dec5eace8a8c6ac15b9dbe0788db7a7d
Jonathan David Livesay <jonl316@fastmail.com>
2019-09-06 10:34:49 -0500

    Got 3 tests working, working now on getting the one nil test working now

--- two-fer.lisp
+++ two-fer.lisp
@@ -5,7 +5,5 @@
 (in-package #:two-fer)
 
 (defun twofer (&optional (name "you"))
-    (if (name nil)
-        ("One for you, one for me.") 
-        (concatenate 'string "One for " name ", one for me."))
+    (concatenate 'string "One for " name ", one for me.")
     )
